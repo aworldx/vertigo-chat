f82c6112d220cb2f8cfad2cb42505f57a978052f
Хасанов Амир Ринатович <akhasanov@ecom.tech>
2026-09-16 14:17:48 +0300

    Release v0.9.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chat.MixProject do
   def project do
     [
       app: :chat,
-      version: "0.8.1",
+      version: "0.9.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
